c4cdb9dfe4988aaa530d7a386ca0a7a27f752b33
GilvanJSousa <gilvan.sousa@4win.com.br>
2025-06-11 16:49:18 -0300

    test: binQuery ok

--- src/test/java/org/br/com/testes/ExecucaoTestes.java
+++ src/test/java/org/br/com/testes/ExecucaoTestes.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "org.br.com.testes.steps",
-        tags = "@zeroAuth",
+        tags = "@binQuery",
         monochrome = false,
         dryRun = false,
         plugin = {
