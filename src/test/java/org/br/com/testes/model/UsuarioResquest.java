23b410602150d2ffd36cbdf87915bed1426d0d98
GilvanJSousa <gilvan.sousa@4win.com.br>
2025-06-17 15:56:19 -0300

    refactor: faker

--- src/test/java/org/br/com/testes/model/UsuarioResquest.java
+++ src/test/java/org/br/com/testes/model/UsuarioResquest.java
@@ -3,9 +3,9 @@ package org.br.com.testes.model;
 import lombok.Builder;
 
 @Builder
-public record UsuarioResquest(
-        String nome,
-        String email,
-        String password,
-        String administrador
-) { }
+public class UsuarioResquest {
+        private String nome;
+        private String email;
+        private String password;
+        private String administrador;
+ }
--- src/test/java/org/br/com/testes/utils/FakerApiData.java
+++ src/test/java/org/br/com/testes/utils/FakerApiData.java
@@ -1,8 +1,8 @@
 package org.br.com.testes.utils;
 
 import com.github.javafaker.Faker;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.extern.apachecommons.CommonsLog;
 import org.br.com.testes.model.UsuarioResponse;
 
@@ -10,42 +10,25 @@ import java.util.Locale;
 
 @CommonsLog
 @Data
+@Builder
 public class FakerApiData {
     private static final Faker faker = new Faker(new Locale("pt-BR"));
-    private static UsuarioResponse usuarioFaker;
 
-    @Getter
-    private static String name;
-    @Getter
-    private static String email;
-    @Getter
-    private static String password;
+    private String nome;
+    private String email;
+    private String senha;
+    private String administrador;
 
     /**
-     * Gera um usuário CMS fake com dados aleatórios ou retorna o usuário padrao.
-     *
-     * @return um objeto UsuarioResponse com dados fictícios
+     * Gera um usuário com dados aleatórios
+     * @return FakerApiData com dados fictícios
      */
-//    public static UsuarioResponse gerarUsuarioFake() {
-//        if (usuarioFaker == null) {
-//            usuarioFaker = new UsuarioResponse(
-//                faker.name().fullName(),
-//                faker.name().username(),
-//                faker.internet().emailAddress(),
-//                gerarSenhaValida()
-//            );
-//        }
-//        return usuarioFaker;
-//    }
-
-    /**
-     * Gera uma senha válida que atende aos requisitos da API.
-     * Requisitos: pelo menos 8 caracteres, uma letra maiúscula e um número.
-     *
-     * @return uma senha válida
-     */
-    private static String gerarSenhaValida() {
-        String senha = faker.internet().password(8, 12) + "A1";
-        return senha;
+    public static FakerApiData gerarUsuarioFake() {
+        return FakerApiData.builder()
+                .nome(faker.name().fullName())
+                .email(faker.internet().emailAddress())
+                .senha("pwd123")
+                .administrador("true")
+                .build();
     }
 }
