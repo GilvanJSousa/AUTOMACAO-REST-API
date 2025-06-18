cf8ee26cf3fc4648e5a64f91bc664c798eff0f29
GilvanJSousa <162615817+GilvanJSousa@users.noreply.github.com>
2025-06-18 13:50:53 -0300

    Atualizar o ExecucaoTestes.java
    ok

--- src/test/java/org/br/com/testes/ExecucaoTestes.java
+++ src/test/java/org/br/com/testes/ExecucaoTestes.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "org.br.com.testes.steps",
         tags = "@FuncionalidadesCarrinhos",
-        monochrome = false,
+        monochrome = true,
         dryRun = false,
         plugin = {
                 "pretty",
