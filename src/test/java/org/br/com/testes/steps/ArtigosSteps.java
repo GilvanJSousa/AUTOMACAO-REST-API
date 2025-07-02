9ce5cdd3536f9997163087ac430130e38d51ea57
GilvanJSSousa <74104232+GilvanS@users.noreply.github.com>
2025-07-01 22:36:20 -0300

    up

--- src/test/java/org/br/com/testes/steps/ArtigosSteps.java
+++ src/test/java/org/br/com/testes/steps/ArtigosSteps.java
@@ -6,7 +6,10 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.br.com.testes.controllers.ArtigosController;
 import org.br.com.testes.utils.LogFormatter;
+import io.qameta.allure.*;
 
+@Epic("Gestão de Artigos")
+@Feature("Cadastro e Manutenção de Artigos")
 public class ArtigosSteps {
 
 	private final ArtigosController artigosController;
@@ -15,30 +18,27 @@ public class ArtigosSteps {
 		this.artigosController = new ArtigosController();
 	}
 
+
 	@Given("que envio uma requisição de cadastro de Artigos")
 	public void queEnvioUmaRequisicaoDeCadastroDeArtigos() throws Exception {
-		LogFormatter.logStep("Enviando requisição de cadastro de artigo");
-		// Cadastrar o artigo (categoria e autor já foram criados no Background)
 		artigosController.cadastrarArtigo();
 	}
 
 	@And("crio uma categoria para os artigos")
 	public void crioUmaCategoriaParaOsArtigos() {
-		LogFormatter.logStep("Criando categoria para os artigos");
 		artigosController.criarCategoriaAntesDoArtigo();
 	}
 
 	@And("crio um artigo para os testes")
 	public void crioUmArtigoParaOsTestes() {
-		LogFormatter.logStep("Criando artigo para os testes");
 		artigosController.cadastrarArtigo();
 	}
 
 	@Then("a API Artigos deve retornar o código de status {int}")
 	public void aApiArtigosDeveRetornarOCodigoDeStatus(int statusCode) {
-		LogFormatter.logStep("Validando status code: " + statusCode);
 		artigosController.validarStatusCodeArtigos(statusCode);
 	}
+
 
 	@When("eu envio a requisição de listar Artigos com autenticação")
 	public void euEnvioARequisicaoDeListarArtigosComAutenticacao() {
@@ -59,6 +59,7 @@ public class ArtigosSteps {
 	public void euEnvioARequisicaoDeDELETEParaOID() {
 		artigosController.excluirArtigoPorId();
 	}
+
 
 	@Then("a API Artigos deve retornar o status code {int} para exclusão")
 	public void aApiArtigosDeveRetornarOStatusCodeParaExclusao(int statusCode) {
